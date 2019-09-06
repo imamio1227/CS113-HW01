b086df4f92bcc97251cd2ea8eb5dcc0e81f60b53
w7315568 <imamio1227@gmail.com>
2019-09-06 00:12:41 -0700

    commit

--- src/edu/miracosta/cs113/NewClue.java
+++ src/edu/miracosta/cs113/NewClue.java
@@ -15,8 +15,11 @@ package edu.miracosta.cs113;
  *
  */
 
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
+
 import model.Theory;
 import model.AssistantJack;
 
@@ -29,11 +32,24 @@ public class NewClue {
      */
     public static void main(String[] args) {
         // DECLARATION + INITIALIZATION
-        int answerSet, solution, murder, weapon, location;
+        int answerSet, solution = -1, murder, weapon, location;
         Theory answer;
         AssistantJack jack;
         Scanner keyboard = new Scanner(System.in);
         Random random = new Random();
+
+        /**
+         * Record incorrect weapon from checkAnswer.
+         */
+        Set<Integer> incorrectWeaponSet = new HashSet<>();
+        /**
+         * Record incorrect location from checkAnswer.
+         */
+        Set<Integer> incorrectLocationSet = new HashSet<>();
+        /**
+         * Record incorrect murder from checkAnswer.
+         */
+        Set<Integer> incorrectMurderSet = new HashSet<>();
 
         // INPUT
         System.out.println("Which theory would like you like to test? (1, 2, 3[random]): ");
@@ -47,7 +63,19 @@ public class NewClue {
             weapon = random.nextInt(6) + 1;
             location = random.nextInt(10) + 1;
             murder = random.nextInt(6) + 1;
-            solution = jack.checkAnswer(weapon, location, murder);
+
+//            if (!incorrectWeaponSet.contains(weapon) && !incorrectLocationSet.contains(location) && !incorrectMurderSet.contains(murder)) {
+            if (!(incorrectWeaponSet.contains(weapon) || incorrectLocationSet.contains(location) || incorrectMurderSet.contains(murder))) {
+                solution = jack.checkAnswer(weapon, location, murder);
+                if (solution == 1) { // weapon is incorrect.
+                    incorrectWeaponSet.add(weapon);
+                } else if (solution == 2) { // location is incorrect
+                    incorrectLocationSet.add(location);
+                } else if (solution == 3) { // murder is incorrect
+                    incorrectMurderSet.add(murder);
+                }
+            }
+
         } while (solution != 0);
 
         answer = new Theory(weapon, location, murder);
